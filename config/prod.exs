2f01a2c1baa8c306da5e688d5b7e06d01565a5c5
Tommaso Patrizi <tommasop@gmail.com>
2020-01-21 10:35:35 +0100

    challenge

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :historia_live, HistoriaLiveWeb.Endpoint,
-  url: [scheme: "https", host: "www.thinkingco.de", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "http", host: "www.thinkingco.de", port: 80],
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
