6af538ae9117e348218c8e5159c66d771424acc1
Tommaso Patrizi <tommasop@gmail.com>
2023-01-10 17:03:23 +0100

    add https

--- config/prod.exs
+++ config/prod.exs
@@ -46,8 +46,8 @@ config :logger, level: :info
 # We also recommend setting `force_ssl` in your endpoint, ensuring
 # no data is ever sent via http, always redirecting to https:
 #
-#     config :historia_live, HistoriaLiveWeb.Endpoint,
-#       force_ssl: [hsts: true]
+config :historia_live, HistoriaLiveWeb.Endpoint,
+  force_ssl: [hsts: true]
 #
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
